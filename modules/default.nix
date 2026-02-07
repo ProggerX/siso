ccc83fb6cb6604c88a43ee5d33aeae9fe71a6e6e
ProggerX <x@proggers.ru>
2026-02-07 20:34:11 +0300

    add more software

--- modules/default.nix
+++ modules/default.nix
@@ -1,3 +1,5 @@
 {
 	imports = [ ./minimal.nix ./gui ./login.nix ];
+
+    boot.supportedFilesystems = [ "ntfs" ];
 }
--- modules/gui/software.nix
+++ modules/gui/software.nix
@@ -3,5 +3,24 @@
 		firefox
 		gedit
 		nemo
+
+		neovim
+		binwalk
+		usbutils
+		pciutils
+		arch-install-scripts
+		grub2
+		efibootmgr
+		gparted
+		btrfs-progs
+		e2fsprogs
+
+		curl
+		wget
+		p7zip
+		btop
+		pv
+		rsync
+		nixos-install-tools
 	];
 }
