5a9653ed316a74e4dedd9e1f7bc2244737416a6b
ProggerX <x@proggers.ru>
2026-02-08 17:53:52 +0300

    fix fontconfig

--- modules/gui/fonts.nix
+++ modules/gui/fonts.nix
@@ -3,7 +3,14 @@
 		noto-fonts
 		noto-fonts-color-emoji
 		nerd-fonts.jetbrains-mono
+		dejavu_fonts
 	];
 	fonts.fontDir.enable = true;
+	fonts.fontconfig.enable = true;
+	fonts.fontconfig.defaultFonts = {
+		emoji = ["Noto Color Emoji"];
+		sansSerif = ["JetBrainsMono NF"];
+		monospace = ["JetBrainsMono NF"];
+	};
 	environment.systemPackages = [ pkgs.fontconfig ];
 }
